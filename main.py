5ae0bcf42571ccdbde499291978063cd10d2560e
scott.lan <scott.lan@taisys.com>
2015-02-17 16:26:29 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#coding: utf-8
+
+import os
+import sys
+
+class Main(object):
+    def __init__(self):
+        self.where = []
+
+    # newLine 或是提取的時候
+    def __repr__(self):
+        self.execute()
+        # make self as a List Object instead of super(Main, self).__repr__()
+        return repr(self.where)
+
+    # 掉用 len() 時候提取
+    def __len__(self):
+        self.execute()
+        return len(self.where)
+
+    # for-loop 調用
+    def __iter__(self):
+        self.execute()
+        return iter(self.where)
+
+    # get 單獨一筆的時候 obj[index], SQL: LIMIT 1 OFFSET idx
+    def __getitem__(self, idx):
+        self.execute(idx)
+        return self.where[idx]
+
+    # 真正的執行 Method
+    def execute(self, idx=None):
+        sql = 'get object for repress'
+        if idx:
+            sql += ' with args: %s' % (repr(idx))
+        print sql
+
+    # 實作串接 API
+    def query(self, args):
+        self.where.append(args)
+        return self
+
+if __name__ == '__main__':
+    app = Main()
+    cache = app.query(2).query(4).query(1)      # execute __repr__ not yet
+    print cache      # execute __repr__
+    import pdb; pdb.set_trace()
